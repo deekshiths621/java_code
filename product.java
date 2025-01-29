30a37dbbb2982d55029c49e5120d4930db198a87
deekshiths621 <deekshiths621@gmail.com>
2025-01-29 09:34:35 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,11 @@
+class product
+  {
+    public static void main(string args[])
+    {
+      int a,b,product;
+      a=9;
+      b=4;
+     product=a*b;
+      System.out.println("* of two no is"=product);
+    }
+  }
